988d98a08efdb7caa471a1c82aa9f6b8caeab86c
ghostbustermx <keconecte@gmail.com>
2024-03-20 17:54:33 -0700

    complete solution

--- EducationSolution/Education.API/Program.cs
+++ EducationSolution/Education.API/Program.cs
@@ -1,11 +1,33 @@
+using Education.Application.Cursos;
+using Education.Persistence;
+using FluentValidation.AspNetCore;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<EducationDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
-builder.Services.AddControllers();
+builder.Services.AddMediatR(typeof(GetCursoQuery.GetCursoQueryHandler).Assembly);
+
+
+
+builder.Services.AddControllers().AddFluentValidation(cfg => cfg.RegisterValidatorsFromAssemblyContaining<CreateCursoCommand>());
+
+builder.Services.AddAutoMapper(typeof(GetCursoQuery.GetCursoQueryHandler));
+
+builder.Services.AddCors(o => o.AddPolicy("corsApp", builder => 
+{
+    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+}));
 
 var app = builder.Build();
 
+app.UseCors("corsApp");
 // Configure the HTTP request pipeline.
 
 app.UseAuthorization();
---
+++ EducationSolution/Education.Persistence/Migrations/20240320235618_EducationMigrationInitial.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Education.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class EducationMigrationInitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cursos",
+                columns: table => new
+                {
+                    CursoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Titulo = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    FechaPublicacion = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    FechaCreacion = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Precio = table.Column<decimal>(type: "decimal(14,2)", precision: 14, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cursos", x => x.CursoId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cursos",
+                columns: new[] { "CursoId", "Descripcion", "FechaCreacion", "FechaPublicacion", "Precio", "Titulo" },
+                values: new object[,]
+                {
+                    { new Guid("37f02456-36b0-4079-b3fe-fd6165e2d4e3"), "Curso de UnitTest para NET core", new DateTime(2024, 3, 20, 16, 56, 18, 417, DateTimeKind.Local).AddTicks(9792), new DateTime(2026, 3, 20, 16, 56, 18, 417, DateTimeKind.Local).AddTicks(9792), 1000m, "Master en UnitTest con CQRS" },
+                    { new Guid("8bb7afab-9933-41d3-862b-30c9d16cab74"), "Curso de Java", new DateTime(2024, 3, 20, 16, 56, 18, 417, DateTimeKind.Local).AddTicks(9766), new DateTime(2026, 3, 20, 16, 56, 18, 417, DateTimeKind.Local).AddTicks(9767), 25m, "Master en Java Spring desde las raices" },
+                    { new Guid("f3b7268c-5bcb-4126-af5b-743d2272a945"), "Curso de c# basico", new DateTime(2024, 3, 20, 16, 56, 18, 417, DateTimeKind.Local).AddTicks(9708), new DateTime(2026, 3, 20, 16, 56, 18, 417, DateTimeKind.Local).AddTicks(9720), 56m, "C# desde cero hasta avanzado" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cursos");
+        }
+    }
+}
